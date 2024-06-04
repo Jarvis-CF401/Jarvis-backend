e0243069e7c2db429602c912fff5acd1b88ba1da
Patrick Kawika Reveira <patrick.k.reveira@gmail.com>
2024-06-03 21:17:12 -0700

    commit to merge ahmed branch

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express');
 const app = express();
+// const cors = require('cors');
 
-app.use(cors());
+// app.use(cors());
 
 app.get('/', (req, res) => {
     res.send('Hello world!')
 })
+
+app.listen(3000, () => {
+    console.log('SERVER IS RUNNING ON PORT 3000')
+})
+export default app;
