20169b3fccf50339f72093a5f6762349ba3f93f9
Patrick Kawika Reveira <patrick.k.reveira@gmail.com>
2024-06-03 22:43:05 -0700

    OpenAI API POST request successful

--- openai-test.js
+++ openai-test.js
@@ -5,7 +5,7 @@ const OpenAI = require('openai')
 const OPENAI_API_KEY = process.env.OPENAI_API_KEY
 
 const openai = new OpenAI({
-    apiKey: process.env.OPENAI_API_KEY  // Ensure you have set this environment variable
+    apiKey: process.env.OPENAI_API_KEY
     });
   
 async function main() {
