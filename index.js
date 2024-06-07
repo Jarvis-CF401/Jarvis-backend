2b9eb11d8482438d96ddf7fcb073e836ee27a8c8
Ahmed Elnogoumi <ahmedelnogoumi@gmail.com>
2024-06-06 22:15:48 -0700

    cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors'); // Import cors package
 const app = express();
 require('dotenv').config();
 const port = process.env.PORT || 3000;
@@ -9,6 +10,13 @@ const authRouter = require('./src/routes/authRouter');
 const errorHandler = require('./src/middlewares/errorHandler');
 
 app.use(bodyParser.json());
+
+// Use CORS middleware
+app.use(cors({
+  origin: 'http://localhost:5173', // Adjust this to your frontend's origin
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  credentials: true, // if you need to send cookies or authentication headers
+}));
 
 app.get('/', (req, res) => {
     res.send('Hello world!');
