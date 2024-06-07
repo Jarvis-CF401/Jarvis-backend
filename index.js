ece17cfa0d6035cf72586cfdedeb591b67d18856
Ahmed Elnogoumi <ahmedelnogoumi@gmail.com>
2024-06-06 22:44:02 -0700

    stashing

--- index.js
+++ index.js
@@ -11,11 +11,11 @@ const errorHandler = require('./src/middlewares/errorHandler');
 
 app.use(bodyParser.json());
 
-// Use CORS middleware
+
 app.use(cors({
-  origin: 'http://localhost:5173', // Adjust this to your frontend's origin
+  origin: 'http://localhost:5173',
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-  credentials: true, // if you need to send cookies or authentication headers
+  credentials: true,
 }));
 
 app.get('/', (req, res) => {
